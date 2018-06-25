b075a8708e1e4847a0952db988696c6c3513952f
Hosung Han <rtigerhan@gmail.com>
2018-06-25 14:01:17 +0900

    get ready to be deployed on heroku

--- index.js
+++ index.js
@@ -28,6 +28,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/snippetRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	//Express will serve up production assets
+	//like our main.js file, or main.css file!
+	app.use(express.static('client/build'));
+
+	//Express will serve up index.html file
+	//if it doesn't recognize the route.
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 //production일때와 개발환경일때 구분
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
